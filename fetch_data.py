672b174d46de06b9ff8299b7bed86c4ad4d75c9d
Akshay Patel <asorathiya367@rku.ac.in>
2022-02-23 14:00:10 +0530

    finel_submit

--- fetch_data.py
+++ fetch_data.py
@@ -7,6 +7,12 @@ try:
     #fetch all data
     for details in rows:
         print(details)
+
+    cur = con.execute("select * from movies where leadactor_name='Sunny Deol'")
+    rows = cur.fetchall()
+    #fetch perticular data
+    for details in rows:
+        print(details)
 except:
     #error in data fetch
     print("Error in data fetch")
